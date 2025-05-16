28d1b77e7e2d1d386680b931db60cd9850ee7712
Hiroki Kashihara <himawarisun1331@gmail.com>
2025-05-16 20:18:04 +0900

    Initial Commit

---
+++ KakeiboMVP2/KakeiboMVP2App.swift
@@ -0,0 +1,20 @@
+//
+//  KakeiboMVP2App.swift
+//  KakeiboMVP2
+//
+//  Created by Hiroki Kashihara on 2025/05/16.
+//
+
+import SwiftUI
+
+@main
+struct KakeiboMVP2App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
