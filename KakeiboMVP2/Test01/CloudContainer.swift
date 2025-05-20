e24b229b715283646d5f9a71ccab82f460f9974a
Hiroki Kashihara <himawarisun1331@gmail.com>
2025-05-20 21:27:25 +0900

    Changes

---
+++ KakeiboMVP2/Test01/CloudContainer.swift
@@ -0,0 +1,8 @@
+//
+//  CloudContainer.swift
+//  KakeiboMVP2
+//
+//  Created by Hiroki Kashihara on 2025/05/20.
+//
+
+import Foundation
---
+++ KakeiboMVP2/Test01/CloudDebugger.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  KakeiboMVP2
+//
+//  Created by Hiroki Kashihara on 2025/05/19.
+//
+
+import Foundation
